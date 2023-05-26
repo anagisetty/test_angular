16f969b688f1198cbff4cf67e2024ce306011cca
dotnet-code <@dotnet-code>
2023-05-26 11:31:54 +0530

    commit from code202305263154

---
+++ testangular/src/app/configure-github/configure-github.component.ts
@@ -0,0 +1,29 @@
+﻿import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+    selector: 'app-configure-github',
+    templateUrl: './configure-github.component.html',
+    styleUrls: ['./configure-github.component.scss']
+})
+export class ConfigureGithubComponent implements OnInit {
+    configureGithubForm: FormGroup;
+
+    constructor(private formBuilder: FormBuilder) { }
+
+    ngOnInit() {
+        this.configureGithubForm = this.formBuilder.group({
+            username: ['', Validators.required],
+            password: ['', Validators.required],
+            url: ['', Validators.required],
+            repoName: ['', Validators.required]
+        });
+    }
+
+    onSubmitConfigureGithub() {
+        if (this.configureGithubForm.valid) {
+            console.log(this.configureGithubForm.value);
+        }
+    }
+
+}
